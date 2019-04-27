3a1d9831f605f5b8ee30f533ecfd24074a5cccdf
Luiz Faitanin <lfaitanin@outlook.com>
2019-04-27 15:20:00 -0300

    Separando os componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Site from './Site';
+import Rodape from './componentes/Rodapes'
+import Cabecalho from './componentes/Cabecalho'
 
-function App() {
+class App extends Component  {
+  render() {
   return (
     <div className="App">
+    <Cabecalho />
         <Site/>
+        <Rodape />
+        
     </div>
   );
+}
 }
 
 export default App;
--- src/componentes/Rodapes.js
+++ src/componentes/Rodapes.js
@@ -6,7 +6,7 @@ const Rodape = props => {
   <a href='#myPage' title='To Top'>
     <span className='glyphicon glyphicon-chevron-up'></span>
   </a>
-  <p>Bootstrap Theme Made By <a href='https://www.w3schools.com' title='Visit w3schools'>www.w3schools.com</a></p>
+  <p>Site criado por <a href='https://github.com/lfaitanin' title="Luiz Faitanin" >https://github.com/lfaitanin</a></p>
 </footer>
     )
 }
